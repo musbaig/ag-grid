113b5e1347270cca8b82fe17c8eb61d27859fd1b
Baig, Muslim <baigm@aetna.com>
2021-01-14 12:08:31 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { renderToString } from 'react-dom/server';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const rowSpan = params => params.data.noOfTreatments >= 1 ? params.data.noOfTreatments : 1;
+	const rowData = [
+		{
+			patient: 'Cluff, Lucy M.',
+			status: 'active',
+			noOfTreatments: 3,
+			treatments: 'Yesterday, July 8',
+			alarmsAlerts: '4 Alert',
+			weight: 150,
+      bp: 171,
+      hr: 234,
+      temp: 96
+		},
+		{
+			patient: 'Cluff, Lucy M.',
+			status: 'active',
+			treatments: '2 days ago, July 7',
+			alarmsAlerts: '2 Alarms',
+			weight: 145,
+      bp: 171,
+      hr: 234,
+      temp: 96
+		},
+		{
+			patient: 'Cluff, Lucy M.',
+			status: 'active',
+			treatments: '9 days ago, June 30',
+			alarmsAlerts: '3 Alarms',
+			weight: 132,
+      bp: 171,
+      hr: 234,
+      temp: 96
+		},
+		{
+			patient: 'Dearson, Perry P.',
+			status: 'active',
+			noOfTreatments: 2,
+			treatments: 'Yesterday, July 8',
+			alarmsAlerts: '1 Alert',
+			weight: 135,
+      bp: 171,
+      hr: 234,
+      temp: 96
+		},
+		{
+			patient: 'Dearson, Perry P..',
+			status: 'active',
+			treatments: '3 days ago, July 5',
+			alarmsAlerts: '1 Alarm',
+			weight: 134,
+      bp: 171,
+      hr: 234,
+      temp: 96
+		},
+		{patient: 'Emerson, Frank J.', status: 'onboarding', noOfTreatments: 0}
+	];
+	const columnDefs = [
+		{headerName: 'Patient', field: 'patient', rowSpan, cellClassRules: {'cell-span': rowSpan}, minWidth: 100},
+		{headerName: 'Status', field: 'status', rowSpan, cellClassRules: {'cell-span': rowSpan}, minWidth: 100},
+		{headerName: 'Treatments', field: 'treatments', minWidth: 100},
+		{headerName: 'Alarms & Alerts', field: 'alarmsAlerts', minWidth: 100},
+		{headerName: 'Weight', field: 'weight', width: 90, suppressSizeToFit: true},
+		{headerName: 'BP', field: 'bp', width: 75, suppressSizeToFit: true},
+		{headerName: 'HR', field: 'hr', width: 75, suppressSizeToFit: true},
+		{headerName: 'Temp', field: 'temp', width: 75, suppressSizeToFit: true}
+	];
+
+	const cars = [
+		{make: 'Tesla', models: [{model:'Model 3'}, {model:'Model Y'}]},
+		{make: 'Lexus', models: [{model:'ES300'}, {model:'LS460'}]}
+	]
+
+	const wtf = params => {
+		console.log(params.value)
+		return renderToString(<div className="ag-theme-alpine" style={{height: 350, width: '100%'}}><AgGridReact rowData={params.value}><AgGridColumn field='model' /></AgGridReact></div>);
+	}
+
+	return (
+		<div className="ag-theme-alpine" style={{height: 350, width: '100%'}}>
+			<AgGridReact gridOptions={{
+				rowData,
+				columnDefs,
+				suppressRowTransform: true
+			}}/>
+			<AgGridReact rowData={cars}>
+				<AgGridColumn field='make' />
+				<AgGridColumn field='models' cellRenderer={params => wtf(params)} />
+			</AgGridReact>
+		</div>
+	);
 }
 
 export default App;
